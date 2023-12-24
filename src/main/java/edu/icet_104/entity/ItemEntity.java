922615ccc02fbbfb40d34131c9677aeebe74e8b6
Dinuk1994 <ddt941119@gmail.com>
2023-12-24 11:43:14 +0530

    Creating Repository and entity

---
+++ src/main/java/edu/icet_104/entity/ItemEntity.java
@@ -0,0 +1,15 @@
+package edu.icet_104.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ItemEntity {
+    private String name;
+    private double price;
+}
---
+++ src/main/java/edu/icet_104/repository/InventoryRepository.java
@@ -0,0 +1,7 @@
+package edu.icet_104.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface InventoryRepository extends CrudRepository {
+
+}
